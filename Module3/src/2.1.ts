d0b04f721dfb5269b1a289305f0b8d30bf38cb6a
emonkhan500 <khdilo47@gmail.com>
2024-11-19 18:36:48 +0600

    first commit

---
+++ Module3/src/2.1.ts
@@ -0,0 +1,39 @@
+{
+    // class -oop
+
+    class Animal{
+        name: string;
+        species: string;
+        sound: string;
+        constructor(name:string, species:string ,sound:string){
+            this.name=name,
+            this.species=species,
+            this.sound=sound
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
