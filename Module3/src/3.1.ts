da6b64a6c3d0e50c376f40657f7a60981bcc3601
emonkhan500 <khdilo47@gmail.com>
2024-11-24 21:54:07 +0600

    first commt

--- Module3/src/3.1.ts
+++ Module3/src/3.1.ts
@@ -1,23 +1,38 @@
 {
     // class -oop
 
+    // class Animal{
+        
+    //     name: string;
+        
+    //     species: string;
+    //     sound: string;
+    //     constructor(name:string, species:string ,sound:string){
+    //         this.name=name,
+    //         this.species=species,
+    //         this.sound=sound
+    //     }
+    //     makeSound(){
+    //         console.log(`The ${this.name} is chillaitase like ${this.sound}`);
+    //     }
+    // }
+
+
     class Animal{
-        
-        name: string;
-        
-        species: string;
-        sound: string;
-        constructor(name:string, species:string ,sound:string){
-            this.name=name,
-            this.species=species,
-            this.sound=sound
+        constructor(public name:string, public species:string, public sound:string){
+
+        }
+        makeSound(){
+            `the ${this.name} is chillaitase like ${this.sound}`
         }
     }
 
+const  dog = new Animal('Kotta', 'Dog', 'ghew ghew')
 
-
-
-
+const  cat = new Animal('Bilay','cat','meaw meaw')
+const catsName= dog.name
+console.log(catsName);
+cat.makeSound()
 
 
 
--- Module3/src/3.2.ts
+++ Module3/src/3.2.ts
@@ -1,4 +1,6 @@
 {
+// oop -inheritence
+
 
 
 
